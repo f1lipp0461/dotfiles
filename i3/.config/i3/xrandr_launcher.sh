ee21509d51b20d1c60e74d74edd52e460b93e560
f1lipp0461 <filippoprotto2004@gmail.com>
2025-08-02 10:55:40 +0200

    small fixes to i3 config

--- i3/.config/i3/xrandr_launcher.sh
+++ i3/.config/i3/xrandr_launcher.sh
@@ -5,19 +5,7 @@ num_outputs=$(echo "$connected_outputs" | wc -l)
 
 sleep 1
 
-if [ "$num_outputs" -eq 1 ]; then
-    for ws in {1..10}; do
-        i3-msg "workspace $ws output HDMI-1"
-    done
-else
+if [ "$num_outputs" -ne 1 ]; then
     i3-msg "exec xrandr --output HDMI-1 --primary --right-of HDMI-0"
-    
-    for ws in {1..5}; do
-        i3-msg "workspace $ws output HDMI-1"
-    done
-
-    for ws in {6..10}; do
-        i3-msg "workspace $ws output HDMI-0"
-    done
 fi
 
