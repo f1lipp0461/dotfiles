0d9f3ac1f6e78e6ae283e8536bcdeaf0da7f16de
f1lipp0461 <filippoprotto2004@gmail.com>
2025-09-04 16:15:04 +0200

    added to polybar a module to switch between keyboard layouts

---
+++ polybar/.config/polybar/scripts/keyboard-layout.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+layouts=("it" "us")
+
+# Legge il layout attuale
+current=$(setxkbmap -query | awk '/layout/{print $2}')
+
+if [[ $1 == "toggle" ]]; then
+    for i in "${!layouts[@]}"; do
+        if [[ "${layouts[$i]}" == "$current" ]]; then
+            next=$(( (i + 1) % ${#layouts[@]} ))
+            setxkbmap "${layouts[$next]}"
+            break
+        fi
+    done
+else
+    echo "${current^^}"
+fi
+
